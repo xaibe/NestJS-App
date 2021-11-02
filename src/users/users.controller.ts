d350e1c5540a1f5b27455d5730c5b6efd5553520
xaibe <sa30967965@gmail.com>
2021-11-02 12:57:47 +0500

    Added Api Responses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,26 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @ApiOkResponse({ type: User, isArray: true })
   @Get('all')
   getusers(): User[] {
     return this.usersService.findAll();
   }
+
+  @ApiOkResponse({ type: User })
   @Get(':id')
   getUserById(@Param('id') id: string): User {
     return this.usersService.findById(Number(id));
   }
-
+  @ApiCreatedResponse({ type: User })
   @Post('create')
   create(@Body() body: CreateUserDto): User {
     return this.usersService.createUser(body);
